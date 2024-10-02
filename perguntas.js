4214c90f6fbe6ec38a65b23b6fb3498817d47909
thamii008 <160048784+thamii008@users.noreply.github.com>
2024-10-02 14:42:01 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,37 @@ criaCartao(
     'Como se diz tchau em Inglês?',
     'tchau em ingles é bye'
 )
+      criaCartao(
+      'Programação',
+    'O que e laranja?',
+    'a laranja e uma fruta'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a cor da banana?',
+    'A cor da banana e amarela'
+)
+
+criaCartao(
+    'Programação',
+    'Quantas cores tem o arco-íris?',
+    'o arco-íris tem 8 cores'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'tchau em ingles é hi'
+)
+      criaCartao(
+      'Programação',
+    ' Qual é o único país onde o animal coala vive?',
+    ' o pais e Austrália'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil e Brasilia'
+)
